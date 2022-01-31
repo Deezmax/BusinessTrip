b5a519a057d520cd2e0a542ac0a2f7462008de9e
Maximilian Schreiter <maximilian.schreiter@t-online.de>
2022-01-31 14:58:35 +0100

    Add sprinboot example

---
+++ springboot-endpoints/src/main/java/de/example/springbootendpoints/SpringbootEndpointsApplication.java
@@ -0,0 +1,13 @@
+package de.example.springbootendpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEndpointsApplication.class, args);
+	}
+
+}
